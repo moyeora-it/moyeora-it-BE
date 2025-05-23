0af6cece0695af39a4234c3cfe4ad0fc963e73ea
minsu <a2238737@gmail.com>
2025-05-23 15:56:04 +0900

    [Feat] 로그아웃

--- router/userRouter.js
+++ router/userRouter.js
@@ -8,6 +8,7 @@ router.post('/signup', userController.signup); // 테스트완
 router.post('/email-auth', userController.FindEmailAuth); // 테스트완
 router.post('/login', userController.login); // 테스트완
 router.post('/check-email-auth', userController.checkEmailAuth); // 테스트완
+router.post('/logout', userController.logout); // 테스트완
 
 router.patch('/delete', jwtToken.accessVerifyToken, userController.deleteUser); // 테스트완
 router.get('/info', jwtToken.accessVerifyToken, userController.userInfo); // 테스트완
